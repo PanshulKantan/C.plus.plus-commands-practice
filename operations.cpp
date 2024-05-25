d4f7fd038bb7f3aabf7ba67fcdbc574234ac4545
PanshulKantan <panshulkantansrivastava@gmail.com>
2024-05-25 23:39:34 +0530

    operations

---
+++ operations.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(){
+
+    int x=10;
+
+    x=x%2;
+
+    /*or x+=1; , x++ (for adding 1 only)
+     x-=1, x--(for subtracting 1 only), x=x-1
+     x*=2(multiply), x/=2(divide)
+     to give out the remainder as an output, use '%' sign
+     BODMAS is used here also*/
+
+    std::cout <<x;  
+}
