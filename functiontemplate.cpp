6fb60e4641c0032c51fd38670b9666b1579a7e19
PanshulKantan <panshulkantansrivastava@gmail.com>
2024-08-25 22:09:45 +0530

    learned some advance topics

--- functiontemplate.cpp
+++ functiontemplate.cpp
@@ -12,6 +12,21 @@ template <typename A, typename U>
 auto maxauto (A a, U b){ //auto is used because if A, or U is used, it will give the output in format of the input with that particular template so auto will give exact data type as output
  return (a>b)? a : b; // ternary operator can be used for different input data types but if else statements cant handle different data types.
 }
+// Note that auto can also be used to just determine the variable type itself
+/*
+for example
+        auto var1 {12}; will assign it as integer
+        auto var2 {2.9}; as double
+        auto var3{2.4320f} as float 
+        auto var4{342.4220L} as long double
+
+
+
+        for integers also,
+        auto var5{123u} u means unsigned
+        auto var6{21314uL} UL is unsigned long
+        auto var7[2143412LL] LL means long long
+*/
 int main(){
  std::cout << maxtemplate(1.3,2.4) << '\n'; // this will accept all types of data but inputs should be of same type
  std::cout << maxauto(1,2.5) << '\n'; //this will accept all data types with different data types of input, BUT will give output of only the type function is, for T, input is of int type and function hence the output will be int only even if U is a double
--- variable.cpp
+++ variable.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip> //used for setprecision
 
 int main () {
 
@@ -16,9 +17,28 @@ int main () {
     int AGE{10}; // this gives the value to the variable as 10
     // also note that int age{2.9} wont be taken and give an error
     int Age(2.9); // this will work but it will implicitely convert the value inside it to integer so 2.9 will chop off 0.9 and will give 2
+    // ALSO NOTE THAT WE CAN CHANGE THE SIGNS OF THE INT VALUE WITH RESPECT TO WHAT WE WANT
+    signed int x{-300}; // value of x is -300
+    unsigned int x{-300}; // this time the value of x is 300 as we want the value unsigned, basically like a mod
+    // short short unsigned int, long signed int, unsigned lon long int, can also be used it just helps saving storage THEY ONLY WORK FOR INTEGERS
 
-    double y = 8.7;
+
+
+    double y = 8.12345;
+    std::cout << std::setprecision(4); //gives 8.123 as it cut everything after 4 digits
+    std::cout << std::fixed;
+    std::cout << std::setprecision(2); // this will only cut everything after 2 digits WHICH ARE after decimals (numbers before decimals wont be harmed)
+
+    std::cout << std::scientific;
+    std::cout << std::setprecision(2); // this will use scientific notation after the number of digits being set
+    // for ex for 8561.23, it will set or 2 and then use scientific notation and give 8.6e+3 where e+3 means 10^3   IMPORTANT
+
+
     // double is used for decimal numbers
+    // there are 2 more variables which represent fractions which are float and long double
+    float a {1.12435436524113220f}; // 0f at the end is the format for float
+    long double b {1.13425244230L}; // 0L is a format for long double
+    // for double, setprecision(value) can cut off the non needed part, for 8.12345,
 
     std::cout << x << std::endl;
     std::cout << y << '\n';
@@ -27,12 +47,19 @@ int main () {
     char grades = 'A';
     /*char is used for a single character
     also, when the variable assigned, use ' ' to define them or else it will show error*/
+    //there are ascii numbers assigned for chars so you can also write char grades = 65 which means A in ascii table
+    // if you cout grades, it will print A.
+    // BUT TO PRINT THE NUMBER ITSELF, std::cout << "grades: " << static_cast<int>(grades) THIS WILL PRINT THE INTEGER ITSELF (65 FOR A)
 
     std::cout << grades << '\n';
 
     bool choice = true;
     bool answer=false;
-    //bool is used for only true or false
+    //bool is used for only true or false. NOTE THAT COMPARISON OPERATORS LIKE >, < OR = WORKS LIKE BOOLEANS
+    std::cout << (2==2); // this is a simple equation as it is true, it will print 1 which is integral value of true
+    std::cout << std::boolalpha; //enabling boolalpha will print true and false instead of 0 or 1 in boolean statements
+    std::cout << (2==2); // this time, BECAUSE OF BOOLALPHA it will print true and not 1, this is the use of it
+    std::cout << std::noboolalpha; //this will disable boolalpha
 
 
     std::string wassup = "Look at my progress bitch";
